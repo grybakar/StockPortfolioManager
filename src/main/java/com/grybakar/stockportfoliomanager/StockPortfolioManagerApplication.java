a0bb53bc7864387240dc782471c607262deb671e
Karolis <Karolisgrybas@outlook.com>
2023-02-10 18:36:28 +0200

    First commit

---
+++ src/main/java/com/grybakar/stockportfoliomanager/StockPortfolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.grybakar.stockportfoliomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPortfolioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortfolioManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grybakar/stockportfoliomanager/StockPortfolioManagerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.grybakar.stockportfoliomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPortfolioManagerApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+		//Later
+	}
+
+}
